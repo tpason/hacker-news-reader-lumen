46bc7d820edb16165f9a8a6ec6861cb8660c207c
sontpa@k.s-cubism.jp <sontpa@k.s-cubism.jp>
2016-11-24 09:24:34 +0700

    CRUD simple

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,3 +16,15 @@ $app->get('/', function () use ($app) {
 });
 $app->get('/{type}', 'HomeController@index');
 $app->get('/', 'HomeController@index');
+
+$app->group(['prefix' => 'api/v1'], function($app)
+{
+    $app->post('car','CarController@createCar');
+
+    $app->put('car/{id}','CarController@updateCar');
+
+    $app->delete('car/{id}','CarController@deleteCar');
+
+    $app->get('car','CarController@index');
+});
+
